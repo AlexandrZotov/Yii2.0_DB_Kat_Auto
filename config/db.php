e8fc4f0fd0f2a50f242a55b266b1f4b07f8ef0f8
AlexandrZotov <alexst2exot@yandex.ru>
2016-03-18 14:04:10 +0300

    Added files via upload

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=kat_auto',
+    'username' => 'root',
+    'password' => '1z2xc3v4',
+    'charset' => 'utf8',
+];
